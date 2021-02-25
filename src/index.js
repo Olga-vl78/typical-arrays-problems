cf8d23336ea763ec77490de552d1778e0775fb5a
Olga-vl78 <olga@butovich.com>
2021-02-25 14:30:58 +0200

    feat: add Typical Arrays Problems solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+   let result = array.reduce((acc, current) => acc < current ? acc : current);
+   return result;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+   let result = array.reduce((acc, current) => acc > current ? acc : current);
+   return result;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+   let result = array.reduce((sum, current) => sum + current)/array.length;
+   return result;
 }
